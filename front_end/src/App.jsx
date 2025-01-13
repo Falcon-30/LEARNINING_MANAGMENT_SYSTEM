e3e4dcd690a8120695b1d99fed1e9e9879330671
Falcon-30 <parmarjainu25@gmail.com>
2025-01-13 19:24:14 +0530

    reset

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,33 +1,14 @@
-import { createBrowserRouter } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Login from './pages/Login'
-import HeroSection from './pages/student/HeroSection'
-import MainLayout from './layout/MainLayout'
-import { RouterProvider } from 'react-router'
 
-const appRouter = createBrowserRouter([
-  {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-      {
-        path: '/',
-        element: (
-          <>
-            <HeroSection />
-          </>
-        )
-      },{
-        path:"login",
-        element:<Login/>
-      }
-    ]
-  }
-])
-function App () {
+import Login from './pages/Login'
+
+function App() {
+  
+
   return (
     <main>
-      <RouterProvider router={appRouter}/>
+      <Login/>
+
+     
     </main>
   )
 }
--- front_end/src/pages/Login.jsx
+++ front_end/src/pages/Login.jsx
@@ -18,7 +18,6 @@ import {
 } from '@/features/api/authApi'
 import { Loader2 } from 'lucide-react'
 import { toast } from 'sonner'
-import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
   const [signUpInput, setSignUpInput] = useState({
@@ -65,7 +64,6 @@ const Login = () => {
     await action(inputData)
   }
 
-  const navigate=useNavigate();
   useEffect(() => {
 
     if(registerSuccess && registerData){
@@ -73,8 +71,6 @@ const Login = () => {
     }
     if(loginSuccess && loginData){
       toast.success(loginData.message || "Login SuccessFul")
-      navigate("/")
-
     }
     if(registerError){
       toast.error(registerError.data.message || "Signup Failed")
@@ -92,7 +88,7 @@ const Login = () => {
     registerError
   ])
   return (
-    <div className='flex items-center justify-center w-full mt-20'>
+    <div className='flex items-center justify-center w-full'>
       <Tabs defaultValue='account' className='w-[400px]'>
         <TabsList className='grid w-full grid-cols-2'>
           <TabsTrigger value='signup'>SignUp</TabsTrigger>
